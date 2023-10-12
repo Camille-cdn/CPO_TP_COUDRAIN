d3f0c14ab4324db3dcae5480b339af1bfb3ccff4
camillecoudrain <camillecoudrain@localhost>
2023-10-12 19:19:46 +0200

    Envoi du TP2 relation 1

---
+++ TP2_relation_1_COUDRAIN/src/tp2_relation_1_coudrain/Personne.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_coudrain;
+
+/**
+ *
+ * @author camillecoudrain
+ */
+public class Personne {
+    String nom;
+    String prenom;
+    
+    public void Personne(String unNom, String unPrenom){
+        unNom = nom;
+        unPrenom = prenom;
+    }
+    
+}
---
+++ TP2_relation_1_COUDRAIN/src/tp2_relation_1_coudrain/Voiture.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_relation_1_coudrain;
+
+/**
+ *
+ * @author camillecoudrain
+ */
+public class Voiture {
+   String Modele;
+   String Marque;
+   int PuissanceCV;
+    
+}
